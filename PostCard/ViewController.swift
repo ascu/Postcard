36e00d4fbd2955d3e3743b5a4eeb884dd30d8d38
Alvin Scudder <alvin.scudder@iqmtel.com>
2014-11-02 13:21:50 +0100

    Initial Commit

---
+++ PostCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PostCard
+//
+//  Created by Alvin Scudder on 02/11/14.
+//  Copyright (c) 2014 Alvin Scudder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
